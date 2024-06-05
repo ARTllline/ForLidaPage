36b7b2e0eda961abb6d73a8ef2092e13f1e61cf0
ARTllline <vedutenkonikita149@gmail.com>
2024-06-05 12:26:43 +0300

    фывфываааааааааа

---
+++ maze.js
@@ -0,0 +1,78 @@
+const canvas = document.getElementById('mazeCanvas');
+const ctx = canvas.getContext('2d');
+
+const maze = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+const cellSize = canvas.width / maze[0].length;
+const player = { x: 1, y: 1 };
+const exit = { x: 18, y: 18 };
+
+function drawMaze() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let y = 0; y < maze.length; y++) {
+        for (let x = 0; x < maze[y].length; x++) {
+            ctx.fillStyle = maze[y][x] === 1 ? 'black' : 'white';
+            ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+        }
+    }
+    // Рисуем выход зеленым цветом
+    ctx.fillStyle = 'green';
+    ctx.fillRect(exit.x * cellSize, exit.y * cellSize, cellSize, cellSize);
+    // Рисуем игрока красным цветом
+    ctx.fillStyle = 'red';
+    ctx.fillRect(player.x * cellSize, player.y * cellSize, cellSize, cellSize);
+}
+
+function moveUp() {
+    if (maze[player.y - 1] && maze[player.y - 1][player.x] === 0) player.y -= 1;
+    checkWin();
+    drawMaze();
+}
+
+function moveDown() {
+    if (maze[player.y + 1] && maze[player.y + 1][player.x] === 0) player.y += 1;
+    checkWin();
+    drawMaze();
+}
+
+function moveLeft() {
+    if (maze[player.y][player.x - 1] === 0) player.x -= 1;
+    checkWin();
+    drawMaze();
+}
+
+function moveRight() {
+    if (maze[player.y][player.x + 1] === 0) player.x += 1;
+    checkWin();
+    drawMaze();
+}
+
+function checkWin() {
+    if (player.x === exit.x && player.y === exit.y) {
+        document.getElementById('nextLevelLab').style.display = 'block';
+    }
+}
+
+window.onload = drawMaze;
